f2063394ad92fa16376c88df01f660567df3735b
artemakimov <86406541+artemakimov@users.noreply.github.com>
2022-09-01 17:09:47 +0300

    Add files via upload

---
+++ src/app/sitePages/authPage/authPage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './authPage.component.html',
+  styleUrls: ['./authPage.component.scss']
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
