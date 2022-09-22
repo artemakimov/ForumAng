a4305f6373b36a2f2d68153017fe4430fae039f6
artemakimov <artyom.akemov@gmail.com>
2022-09-22 07:00:21 +0300

    hotfixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyC-Y6oMRW_VJjPPcdLw6fXmZmQgJq6ITls',
-    authDomain: 'forum-3b8b6.firebaseapp.com',
-    projectId: 'forum-3b8b6',
-    storageBucket: 'forum-3b8b6.appspot.com',
-    messagingSenderId: '902338458930',
-    appId: '1:902338458930:web:6519d0531a9be94704932b',
+    apiKey: "AIzaSyC-Y6oMRW_VJjPPcdLw6fXmZmQgJq6ITls",
+    authDomain: "forum-3b8b6.firebaseapp.com",
+    projectId: "forum-3b8b6",
+    storageBucket: "forum-3b8b6.appspot.com",
+    messagingSenderId: "902338458930",
+    appId: "1:902338458930:web:6519d0531a9be94704932b",
+    measurementId: "G-S3FJNGRW37"
   },
 };
 
