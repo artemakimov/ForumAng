b4626cd289134943418d95beae2665b14fdc9aef
ekalka <artyom.akemov@gmail.com>
2022-11-22 19:16:57 +0300

    add: comments; fix: comment method

--- src/app/core/models/interfaces/post.interface.ts
+++ src/app/core/models/interfaces/post.interface.ts
@@ -1,7 +1,10 @@
+import {Comment} from "../interfaces/comment.interface";
+
 export interface Post {
     id: string;
     title: string;
     text: string;
     date: Date;
     tags: string[];
+    comments: Comment[];
   }
--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { Post } from 'src/app/core/models/interfaces/post.interface';
 import { PostService } from 'src/app/core/services/post.service';
-
+import { Comment } from 'src/app/core/models/interfaces/comment.interface'
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
@@ -13,27 +14,51 @@ export class PostComponent implements OnInit {
   private destroy = new Subject<boolean>();
   public postId: string;
   public post: Post;
+  public form: FormGroup;
+  public comments: Comment[];
   constructor(
     private activatedRoute: ActivatedRoute,
-    private postService: PostService
+    private postService: PostService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.getId();
+    this.initForm();
     this.postService
       .getPost(this.postId)
       .pipe(takeUntil(this.destroy))
       .subscribe((post) => {
         this.post = post;
+        this.comments = post.comments;
       });
 
-      
   }
 
   public getId(): void {
     this.activatedRoute.params.pipe(takeUntil(this.destroy)).subscribe(
       (param) => (this.postId = Object.values(param)[0])
     );
+  }
+
+  public initForm(): void {
+    this.form = this.formBuilder.group({
+      commentText: ['']
+    });
+  }
+
+  public onSubmit(): void {
+    const commentObj = {
+        text: this.form.get('commentText').value,
+        date: new Date(),
+    }
+    this.postService.createComment(this.postId, commentObj)
+      .pipe(takeUntil(this.destroy))
+      .subscribe((id) => {
+        const comment = commentObj as Comment;
+        comment.id = Object.values(id)[0];
+        this.form.reset();
+      });
   }
 
   ngOnDestroy() {
